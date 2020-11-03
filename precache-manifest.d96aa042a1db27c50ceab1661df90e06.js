12f62cade5af6624b0442ae193bcc7d1a1a90b6e
kranthikumar-muske <kranthikumar.muske@gmail.com>
2020-11-02 20:53:17 -0600

    Updates

---
+++ precache-manifest.d96aa042a1db27c50ceab1661df90e06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68975eb4fe0abe3d4e3f",
+    "url": "/MyPortfolio/static/css/main.0641f76d.chunk.css"
+  },
+  {
+    "revision": "68975eb4fe0abe3d4e3f",
+    "url": "/MyPortfolio/static/js/main.68975eb4.chunk.js"
+  },
+  {
+    "revision": "ae5132617e7538071453",
+    "url": "/MyPortfolio/static/js/runtime~main.ae513261.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/MyPortfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "86ffc8f52ecb036bf5f6ca7ca79c72a1",
+    "url": "/MyPortfolio/index.html"
+  }
+];
